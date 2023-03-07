a6eafbd7ce59cbc0ba52c8ba881908c979ad320f
rammallireddy <126081396+rammallireddy@users.noreply.github.com>
2023-03-06 22:20:21 -0600

    Add files via upload

---
+++ week3/day1/college/College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		
+		System.out.println("The college name is VKR and VNB");
+	}
+	
+	public void collegeCode()
+	{
+		System.out.println("College code is VKRVNB");
+	}
+	public void collegeRank() {
+		
+		System.out.println("All india college Rank is 1");
+	}
+}
+
