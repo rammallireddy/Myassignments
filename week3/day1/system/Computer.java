321333e3c8b3fe60bb6751253395188f27b58c03
rammallireddy <126081396+rammallireddy@users.noreply.github.com>
2023-03-06 22:21:25 -0600

    Add files via upload

---
+++ week3/day1/system/Computer.java
@@ -0,0 +1,12 @@
+package org.system;
+
+public class Computer {
+
+	
+	public void computerModel() {
+		
+		System.out.println("the compuet model is HP");
+	}
+	
+
+}
---
+++ week3/day1/system/Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public void desktopSize()
+	{
+		System.out.println("the computer desktop size is 64bit");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop des= new Desktop();
+		des.desktopSize();
+		des.computerModel();
+
+	}
+
+}
