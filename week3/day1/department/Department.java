ee2fa805af6577dabc48db2275af022a973d5270
rammallireddy <126081396+rammallireddy@users.noreply.github.com>
2023-03-06 22:20:41 -0600

    Add files via upload

---
+++ week3/day1/department/Department.java
@@ -0,0 +1,15 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College{
+
+	public void deptName()
+{
+		
+System.out.println("The Dept name is CSE");
+}
+public static void main(String[] args) {
+	
+}	
+}
